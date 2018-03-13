aa99d3665feb1a93ab1dc2f309347f09127e8c1f
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-13 07:37:11 +0530

    added event count

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -9,31 +9,70 @@ import { FirebaseService } from '../../services/firebase.service';
 })
 export class HomeComponent {
   allEvents;
-  objectKeys=Object.keys;
+  objectKeys = Object.keys;
   parseDate = Date.parse;
   todayTimestamp = Date.parse((new Date()).toString());
   Math: any;
-  constructor(private db:AngularFireDatabase,
-              private fs:FirebaseService){
-  this.Math=Math;     
-  
-  
+  totalCount = 0;
+  countFb = 0;
+  countNoddy = 0;
+  countEventsHigh = 0;
+
+
+  constructor(private db: AngularFireDatabase,
+    private fs: FirebaseService) {
+    this.Math = Math;
+
+
   }
-  ngOnInit(){
-    this.fs.returnItems("events").valueChanges().subscribe(data=>{
-      this.allEvents=data || [];
-      console.log(data);
+  ngOnInit() {
+    this.fs.returnItems("events").valueChanges().subscribe(data => {
+      this.allEvents = data || [];
+      
+      // console.log(data);
+
     });
   }
-  purgeAll(){
-    // this.fs.removeAll();
+  ngAfterViewInit(){
+    let keys = Object.keys(this.allEvents);
+    this.totalCount = keys.length;
+    keys.forEach((key) => {
+      if (this.allEvents[key].mySource == 'fb') {
+        this.countFb++;
+      }
+      if (this.allEvents[key].mySource == 'eventsHigh') {
+        this.countEventsHigh++;
+      }
+      if (this.allEvents[key].mySource == 'noddysApp') {
+        this.countNoddy++;
+      }
+    })
+    // console.log(this.allEvents);
+    console.log(this.totalCount);
+    console.log(this.countFb);
+    console.log(this.countEventsHigh);
+    console.log(this.countNoddy);
   }
   
-  purge(originName){
+  purgeAll() {
+    // this.fs.removeAll();
+  }
+
+  purge(originName) {
     this.fs.removeEventByOrigin(originName);
   }
-  purgeEvent(id){
+  purgeEvent(id) {
     console.log(id);
+    if(this.allEvents[id].mySource=='fb'){
+      this.countFb--;
+    }
+    if(this.allEvents[id].mySource=='eventsHigh'){
+      this.countEventsHigh--;
+    }
+    if(this.allEvents[id].mySource=='noddysApp'){
+      this.countNoddy--;
+    }
     this.fs.removeEventById(id);
+    this.totalCount--;
   }
 }
